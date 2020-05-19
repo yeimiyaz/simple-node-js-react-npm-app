0297af933957d055437deafa27246206393feece
dhruvinparikh93 <imsilent93@gmail.com>
2020-05-19 13:44:42 -0400

    add info for docker-toolbox

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,6 +24,7 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
+# If you run on Docker-Toolbox, instead of localhost, use docker-machine's ip
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
